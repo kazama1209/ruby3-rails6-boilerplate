6cf90e36f987d4b9a8323c785c90d9049801b804
Takashiro Kazama <kazamanbou1209.pfm@gmail.com>
2021-12-06 00:08:36 +0900

    Install Bootstrap & fontawesome

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
 
-module.exports = environment
+const webpack = require("webpack");
+environment.plugins.prepend(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery/src/jquery",
+    jQuery: "jquery/src/jquery",
+    Popper: [
+      "popper.js",
+      "default",
+    ] /* 多くの記事で 「$ yarn add popper.js」と書いてあるが、実際は「$ yarn add @popperjs/core」じゃないと動かない */,
+  })
+);
+
+module.exports = environment;
