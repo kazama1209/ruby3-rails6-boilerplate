8eed36a4f0275296627f6cf2fb58e79d01c69352
Takashiro Kazama <kazamanbou1209.pfm@gmail.com>
2021-12-06 00:48:19 +0900

    Install Rspec

---
+++ spec/requests/home_controller_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe HomeController, type: :request do
+  describe "GET #index" do
+    it "responds successfully" do
+      get root_path
+
+      expect(response).to have_http_status(200)
+    end
+  end
+end
