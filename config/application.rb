8c62a7f067dc261eda084f27da02b94e5c21be26
Takashiro Kazama <kazamanbou1209.pfm@gmail.com>
2021-12-05 23:21:24 +0900

    Set timezone(Tokyo)

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,9 @@ module Myapp
       g.helper false
     end
 
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
